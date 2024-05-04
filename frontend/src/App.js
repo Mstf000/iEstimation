cabdfa2443c8ce696a927f1690fc6f0fed88f35c
Mstf000 <mostafacrash400@gmail.com>
2024-05-04 21:51:51 +0300

    added firebase config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,23 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'
 import 'firebase/auth'
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionDate } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB-AsmvRhQmq2LxiUtwN11gNuXm5WiW6lg",
+  authDomain: "password-challenge.firebaseapp.com",
+  projectId: "password-challenge",
+  storageBucket: "password-challenge.appspot.com",
+  messagingSenderId: "145968987775",
+  appId: "1:145968987775:web:09037799273550b6e6094a",
+  measurementId: "G-H8JDJVZ7GX"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
