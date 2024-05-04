b672142d459dad3efcbfa453b35425df07b70c2e
Mstf000 <mostafacrash400@gmail.com>
2024-05-04 21:42:26 +0300

    starting version

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
